fc4ce943c31a9c901cbedbd741a7a776cba9430f
himans93 <69123971+himans93@users.noreply.github.com>
2026-07-03 09:23:06 -0400

    ai-response

--- ex-1-print-digits.cpp
+++ ex-1-print-digits.cpp
@@ -1,32 +1,25 @@
 #include <iostream>
 #include <string>
-#include <stack>
 
 int main()
 {
-    //std::cout<<"Hello World";
     int n = 1234;
-    std::stack<int> st;
-    while(n>0){
-        int digit = n%10;
-        std::cout << "d:" << digit << std::endl;
-        st.push(digit);
-        //int new_n = (n-digit)/10;
-        n = n/10;
-        //std::cout << "n:" << new_n << std::endl;
-        //n = new_n;
+
+    std::string input = std::to_string(n);
+    std::string result;
+
+    for (char ch : input)
+    {
+        int digit = ch - '0';
+        int square = digit * digit;
+
+        result += std::to_string(square);
     }
-    
-    std::string s{};
-    while(!st.empty() ) {
-        int digit = st.top();
-        st.pop();
-        s += std::to_string(digit*digit);
-        std::cout << "square-list:" << s << std::endl;
-    }
-    
-    int final = std::stoi(s);
-    std::cout << "result:" << final << std::endl;
+
+    std::cout << "result string: " << result << std::endl;
+
+    int final = std::stoi(result);
+    std::cout << "result int: " << final << std::endl;
 
     return 0;
 }
