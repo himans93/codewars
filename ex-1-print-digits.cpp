ea033327b49ac28fddc77c8eee3de8c315b80bf2
himans93 <69123971+himans93@users.noreply.github.com>
2026-07-03 09:16:23 -0400

    next stage
    use of std::stack
    use of  n/10 instead of n=n-(n%10)*10
    instead storing square, storing the digits and coverting to square at the end

--- ex-1-print-digits.cpp
+++ ex-1-print-digits.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
+#include <string>
+#include <stack>
 
 int main()
 {
     //std::cout<<"Hello World";
     int n = 1234;
+    std::stack<int> st;
     while(n>0){
         int digit = n%10;
         std::cout << "d:" << digit << std::endl;
-        int new_n = (n-digit)/10;
-        std::cout << "n:" << new_n << std::endl;
-        n = new_n;
+        st.push(digit);
+        //int new_n = (n-digit)/10;
+        n = n/10;
+        //std::cout << "n:" << new_n << std::endl;
+        //n = new_n;
+    }
+    
+    std::string s{};
+    while(!st.empty() ) {
+        int digit = st.top();
+        st.pop();
+        s += std::to_string(digit*digit);
+        std::cout << "square-list:" << s << std::endl;
     }
 
     return 0;
